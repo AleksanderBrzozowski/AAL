b61989c85660a229fb96f018f6427dc75b3f95e5
Aleksander <olek.brzozowski@gmail.com>
2016-12-28 22:24:16 +0100

    Add simple test in main.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
+
+
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include "TimerSkipList.h"
+
 int main() {
+    std::ofstream frontOut("frontOut.txt");
+    std::ofstream insertOrUpdateOut("insertOrUpdate.txt");
+    SkipList skipList;
+    TimerSkipList timerSkipList(skipList, frontOut, insertOrUpdateOut);
+
+    for (int i = 20000000; i > 0; --i) {
+        skipList.insertOrUpdate(i, "a");
+    }
+
+    timerSkipList.insertOrUpdate(1205410, "a");
+    timerSkipList.insertOrUpdate(12054504, "a");
+    timerSkipList.insertOrUpdate(1534524504, "a");
+
+
+    frontOut.close();
+    insertOrUpdateOut.close();
     return 0;
 }
